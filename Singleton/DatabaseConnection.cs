c9dde2f4e21fd03b1db662f903e543c92458bc5f
amingolmahalle <a.golmahalleh@alibaba.ir>
2021-06-11 21:31:02 +0430

    Singleton

--- Singleton/DatabaseConnection.cs
+++ Singleton/DatabaseConnection.cs
@@ -1,7 +1,36 @@
+using System;
+
 namespace Singleton
 {
     public class DatabaseConnection
     {
-        
+        private static DatabaseConnection _instance;
+
+        private static readonly object Lock = new object();
+
+        private DatabaseConnection()
+        {
+            //TODO: Enter the code to connect to the database here.
+        }
+
+        public static DatabaseConnection GetInstance()
+        {
+            if (_instance != null)
+                return _instance;
+
+            lock (Lock)
+            {
+                Console.WriteLine("* create new Instance! *");
+
+                _instance ??= new DatabaseConnection();
+            }
+
+            return _instance;
+        }
+
+        public string GetQuery(string query)
+        {
+            return $"execute {query}";
+        }
     }
 }
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Singleton
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("First Call:");
+            DatabaseConnection dbConn1 = DatabaseConnection.GetInstance();
+            string result1 = dbConn1.GetQuery("query1");
+            Console.WriteLine(result1);
+            Console.WriteLine();
+
+            Console.WriteLine("Second Call:");
+            DatabaseConnection dbConn2 = DatabaseConnection.GetInstance();
+            string result2 = dbConn2.GetQuery("query2");
+            Console.WriteLine(result2);
+            Console.WriteLine();
+
+            Console.WriteLine("Third Call:");
+            DatabaseConnection dbConn3 = DatabaseConnection.GetInstance();
+            string result3 = dbConn3.GetQuery("query3");
+            Console.WriteLine(result3);
+            Console.WriteLine();
+
+            Console.WriteLine(dbConn1 == dbConn2);
+            Console.WriteLine(dbConn1 == dbConn3);
+            Console.WriteLine(dbConn2 == dbConn3);
         }
     }
 }
